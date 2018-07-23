9269303e8c1988f70a38dea27d390a88557e9601
Michael Manley <mmanle01@gmail.com>
2018-07-23 17:09:05 -0400

    Added the car data as per the assignment.

---
+++ DecisionTreeAlgorithm/CarData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecisionTreeAlgorithm
+{
+    class CarData
+    {
+        public static string[][] GetData()
+        {
+            List<string[]> retData = new List<string[]>();
+            string path = @"C:\Users\mmanl\Documents\CarData\car.data";
+            if (File.Exists(path))
+            {
+                var lines = File.ReadAllLines(path);
+                foreach (var line in lines)
+                {
+                    var tokens = line.Split(',');
+                    retData.Add(tokens);
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (retData.Count > 10)
+                {
+                    foreach (string item in retData[i])
+                    {
+                        Console.WriteLine(item + ", " );
+                    }
+                    Console.WriteLine();
+                }
+            }
+            return retData.ToArray();
+        }
+
+        
+
+
+    }
+}
--- DecisionTreeAlgorithm/Program.cs
+++ DecisionTreeAlgorithm/Program.cs
@@ -20,25 +20,33 @@ namespace DecisionTreeAlgorithm
             new string[] {"small", "red", "wedge", "no"}
         };
 
-        private static string[] sizeValues = new string[] { "small", "medium", "large" };
-        private static Attribute size = new Attribute("size", sizeValues.ToList(), 0);
+        private static string[] buyingValues = new string[] { "vhigh", "high", "med", "low" };
+        private static Attribute buying = new Attribute("buying", buyingValues.ToList(), 0);
 
-        private static string[] colorValues = new string[] { "blue", "red", "green", "brown" };
-        private static Attribute color = new Attribute("color", colorValues.ToList(), 1);
+        private static string[] maintValues = new string[] { "vhigh", "high", "med", "low" };
+        private static Attribute maint = new Attribute("maint", maintValues.ToList(), 1);
 
-        private static string[] shapeValues = new string[] { "brick", "sphere", "pillar", "wedge" };
-        private static Attribute shape = new Attribute("shape", shapeValues.ToList(), 2);
+        private static string[] doorsValues = new string[] { "2", "3", "4", "5more" };
+        private static Attribute doors = new Attribute("doors", doorsValues.ToList(), 2);
+
+        private static string[] personsValues = new string[] { "2", "3", "more" };
+        private static Attribute persons = new Attribute("persons", personsValues.ToList(), 3);
+
+        private static string[] lug_boot_values = new string[] { "small", "med", "big" };
+        private static Attribute lug_boot = new Attribute("lug_boot", lug_boot_values.ToList(), 4);
+
+        private static string[] safetyValues = new string[] { "low", "med", "high" };
+        private static Attribute safety = new Attribute("safety", safetyValues.ToList(), 5);
 
 
-
-        static string[] classes = new string[] { "yes", "no" };
+        static string[] classes = new string[] { "unacc", "acc", "good", "vgood" };
 
         static void Main(string[] args)
         {
-
-            DecisionTree decisionTree = new DecisionTree(classes, data);
+            string[][] theData = CarData.GetData();
+            DecisionTree decisionTree = new DecisionTree(classes, theData);
             decisionTree.CreateTree();
-            Console.WriteLine(decisionTree.Classify(new string[] { "medium", "green", "pillar" }));
+            Console.WriteLine(decisionTree.Classify(new string[] { "vhigh", "vhigh", "3" , "2", "med", "high"}));
             Console.ReadKey();
         }
 
